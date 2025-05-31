84619f80b4313668fc48c6f02f7de597a5adf390
Thomas Ingram <thomas@taingram.org>
2025-05-31 01:24:52 -0400

    Add RSS icon macro

--- publish.el
+++ publish.el
@@ -16,29 +16,22 @@
 (require 'ox-publish)
 (require 'htmlize)
 
-(setq org-export-with-section-numbers nil
-      org-export-with-toc nil
 
-      ;; ISO8601 Date Format
-      org-export-date-timestamp-format "%Y-%m-%d"
-      org-html-metadata-timestamp-format "%Y-%m-%d"
+;; (setq org-export-date-timestamp-format "%Y-%m-%d")
 
-      ;; Enable HTML5
-      org-html-html5-fancy t
-      org-html-doctype     "html5"
+(setq org-html-htmlize-output-type 'css)
 
-      ;; TODO: tweak headline id format. I dislike the randomly generated ones.
-      ;; Look into customizing: :html-format-headline-function and
-      ;; org-html-format-headline-function
-      ;; org-html-self-link-headlines 'nil
+;; ISO8601 Date Format
+(setq org-html-metadata-timestamp-format "%Y-%m-%d")
 
-      org-html-htmlize-output-type 'css)
 
-(setq org-export-global-macros
-      '(("timestamp" . "@@html:<span class=\"timestamp\">[$1]</span>@@")))
+(add-to-list 'org-export-global-macros
+	     '("timestamp" . "@@html:<span class=\"timestamp\">[$1]</span>@@"))
 
-;; Render ~code~ as kbd tag in HTML
-(add-to-list 'org-html-text-markup-alist '(code . "<kbd>%s</kbd>"))
+(add-to-list 'org-export-global-macros
+	     '("rss" . "@@html:<span class=\"rss-badge\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 448 512\"><path d=\"M128.081 415.959c0 35.369-28.672 64.041-64.041 64.041S0 451.328 0 415.959s28.672-64.041 64.041-64.041 64.04 28.673 64.04 64.041zm175.66 47.25c-8.354-154.6-132.185-278.587-286.95-286.95C7.656 175.765 0 183.105 0 192.253v48.069c0 8.415 6.49 15.472 14.887 16.018 111.832 7.284 201.473 96.702 208.772 208.772.547 8.397 7.604 14.887 16.018 14.887h48.069c9.149.001 16.489-7.655 15.995-16.79zm144.249.288C439.596 229.677 251.465 40.445 16.503 32.01 7.473 31.686 0 38.981 0 48.016v48.068c0 8.625 6.835 15.645 15.453 15.999 191.179 7.839 344.627 161.316 352.465 352.465.353 8.618 7.373 15.453 15.999 15.453h48.068c9.034-.001 16.329-7.474 16.005-16.504z\"></path></svg></span>@@ $1"))
+
+
 
 (defvar taingram--head
   "<link rel=\"stylesheet\" href=\"/style.css\" type=\"text/css\"/>
@@ -61,8 +54,6 @@
 <h1 class=\"title\">%t</h1>
 <div class=\"publish-date\">Published: <span class=\"timestamp\">%d</span></div>
 </header>")
-
-
 
 (defun taingram--gen-footer (&optional comment)
   "Automatically generate footer text and optionally show COMMENT section."
